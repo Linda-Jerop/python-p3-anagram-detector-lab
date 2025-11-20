5523f3412970719ba0eb91c4462145aed5a26413
Linda <jeropkoimur@gmail.com>
2025-11-20 08:43:38 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, possible_anagrams):
+        matches = []
+        for candidate in possible_anagrams:
+            if sorted(candidate.lower()) == sorted(self.word.lower()):
+                matches.append(candidate)
+        return matches
